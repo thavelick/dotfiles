f01a2cf5ee9c07c055a0771cf1d231fdc261c3dd
Tristan Havelick <tristan@havelick.com>
2021-11-15 21:42:13 -0700

    feat: add simple script to install arch packages

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if grep -qF Arch /etc/issue; then
+  missing_packages=`comm  -13 <(pacman -Qq | sort) <(sort arch/packages)`
+  if ! pacman -Qq yay > /dev/null; then
+    sudo pacman -S --needed git base-devel
+    mkdir -p ~/src
+    [[ -d ~/src/yay ]] && git clone https://aur.archlinux.org/yay.git ~/src/yay
+    cd ~/src/yay
+    makepkg -si
+  fi 
+  if [[ -n $missing_packages ]]; then 
+    yay -S $missing_packages
+  fi
+fi

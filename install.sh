cf0be70a1091b3cce6cab81a025d042b0990e239
Tristan Havelick <tristan@havelick.com>
2021-11-17 21:28:02 -0700

    refactor: use better way to call external commands, use env variables instead of ~

--- install.sh
+++ install.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
-export DOTFILES_HOME=`pwd`
+export DOTFILES_HOME=$(pwd)
 if grep -qF Arch /etc/issue; then
-  missing_packages=`comm  -13 <(pacman -Qq | sort) <(sort arch/packages)`
+  missing_packages=$(comm  -13 <(pacman -Qq | sort) <(sort arch/packages))
   if ! pacman -Qq yay > /dev/null; then
     sudo pacman -S --needed git base-devel
-    mkdir -p ~/src
-    [[ -d ~/src/yay ]] && git clone https://aur.archlinux.org/yay.git ~/src/yay
-    cd ~/src/yay
+    mkdir -p $HOME/src
+    [[ -d $HOME/src/yay ]] && git clone https://aur.archlinux.org/yay.git ~/src/yay
+    cd $HOME/src/yay
     makepkg -si
   fi 
   if [ -n $missing_packages ]; then 
     yay -S $missing_packages
   fi
 fi
-
 
 git/setup.sh

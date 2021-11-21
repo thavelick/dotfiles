a7700c63d80df99ff0a5730b4f830b94789cdba4
Tristan Havelick <tristan@havelick.com>
2021-11-21 12:10:08 -0700

    feat: check if folders exist to avoid warnings

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+oh_my_zsh_path=$HOME/.oh-my-zsh 
 # install oh my zsh
-/bin/sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
+[ ! -d $oh_my_zsh_path ] && /bin/sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
 
 # install p10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+p10k_path=${ZSH_CUSTOM:-$oh_my_zsh_path}/custom/themes/powerlevel10k
+[ ! -d $p10k_path ] && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $p10k_path 
 
 # symlink .zshrc
 ln -svf $DOTFILES_HOME/zsh/zshrc $HOME/.zshrc

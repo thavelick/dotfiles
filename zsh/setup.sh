7d4ee9cf11640dbf36cffd653ada8f68e91a2850
Tristan Havelick <tristan@havelick.com>
2021-12-04 12:39:02 -0700

    record history in cahce. Also cd to home on start to override windows junk

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -3,6 +3,9 @@
 zsh_plugins_path=$HOME/.zsh
 mkdir -p $zsh_plugins_path
 
+# Make the cache folder for history
+mkdir -p "$HOME/.cache/zsh"
+
 # install p10k
 p10k_path=$zsh_plugins_path/powerlevel10k
 [ ! -d $p10k_path ] && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $p10k_path

c3549fd6649b11b448bf67e1abb2a0ab72e55e15
Tristan Havelick <tristan@havelick.com>
2021-12-04 11:37:02 -0700

    refactor: remove ohmyzsh dependency

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-oh_my_zsh_path=$HOME/.oh-my-zsh 
-# install oh my zsh
-[ ! -d $oh_my_zsh_path ] && /bin/sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
+zsh_plugins_path=$HOME/.zsh
+mkdir -p $zsh_plugins_path
 
 # install p10k
-p10k_path=${ZSH_CUSTOM:-$oh_my_zsh_path}/custom/themes/powerlevel10k
-[ ! -d $p10k_path ] && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $p10k_path 
+p10k_path=$zsh_plugins_path/powerlevel10k
+[ ! -d $p10k_path ] && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $p10k_path
 
 # symlink .zshrc
 ln -svf $DOTFILES_HOME/zsh/zshrc $HOME/.zshrc

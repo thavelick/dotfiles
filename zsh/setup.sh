7ab1939ced6470d9124c6f5d146dda5b9d3c98ef
Tristan Havelick <tristan@havelick.com>
2021-12-06 22:45:14 -0700

    feat: add zsh syntax highlighting

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -8,7 +8,14 @@ mkdir -p "$HOME/.cache/zsh"
 
 # install p10k
 p10k_path=$zsh_plugins_path/powerlevel10k
-[ ! -d $p10k_path ] && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $p10k_path
+[ ! -d $p10k_path ] && \
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $p10k_path
+
+# install syntax highlighting
+syntax_highlight_path=$zsh_plugins_path/zsh-syntax-highlighting
+[ ! -d $syntax_highlight_path ] && \
+	git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git \
+	$syntax_highlight_path
 
 # symlink .zshrc
 ln -svf $DOTFILES_HOME/zsh/zshrc $HOME/.zshrc

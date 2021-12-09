8ca69ef4e1aa4ffd6ae7a3a744694804307bbdbd
Tristan Havelick <tristan@havelick.com>
2021-12-08 20:22:22 -0700

    feat: add simple history base autosuggestions

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -17,6 +17,12 @@ syntax_highlight_path=$zsh_plugins_path/zsh-syntax-highlighting
 	git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git \
 	$syntax_highlight_path
 
+# install autosuggestions
+suggestions_path=$zsh_plugins_path/zsh-autosuggestions
+[ ! -d $suggestions_path ] && \
+	git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git \
+	$suggestions_path
+
 # symlink .zshrc
 ln -svf $DOTFILES_HOME/zsh/zshrc $HOME/.zshrc
 

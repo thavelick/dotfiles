e0e5b109abb1b24d1b3fb507595bb0d484a2edab
Tristan Havelick <tristan@havelick.com>
2022-01-10 15:24:24 -0700

    feat: move to the next sprint after friday noon

--- work/github.zsh
+++ work/github.zsh
@@ -4,12 +4,16 @@ milestone() {
   unixtime=$(strftime %s)
   hour=$(strftime %k $unixtime)
   day=$(strftime %w $unixtime)
-  [ $day = 5 ] && [ $hour > 12 ] && unixtime=($unixtime - 24*60*60)
+
+  # if it's after friday at noon, add enough days to be sure we count as in next week
+  [[ $day = 5  && $hour -gt 12 || $day -gt 5 ]] && echo "inc" && ((unixtime=$unixtime + 3*24*60*60))
+
   year=$(strftime %Y $unixtime)
   # from: 
   # https://stackoverflow.com/questions/29931375/how-to-use-unix-date-function-to-calculate-quarter-of-the-year
   quarter=$(($(($((10#$(strftime %m $unixtime))) - 1)) / 3 + 1))
-  week=$(expr $(strftime %V $unixtime) + 1)
+  week=$(expr $(strftime %U $unixtime) + 1)
+
   milestone="$year-Q$quarter-$week"
   echo $milestone
 }

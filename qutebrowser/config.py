63921ec5d25c4c335cde4b1a3b8d491828926b68
Tristan Havelick <tristan@havelick.com>
2022-05-22 21:29:07 -0600

    add commands to redirect to privacy/nojs friendly versions of common sites

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -149,7 +149,8 @@ config.set('content.javascript.enabled', True, 'qute://*/*')
 
 
 js_sites = [
-    'http://localhost:8181',
+    'http://localhost:8050', # Miniflux
+    'http://localhost:8181', # Kiwix
     'https://github.com',
     'https://social.linux.pizza',
     'https://duckduckgo.com',
@@ -185,23 +186,24 @@ config.set('url.searchengines', {
 
 
 config.set('zoom.default', '115')
-
+config.set('spellcheck.languages', ['en-US'])
 
 # Bindings for normal mode
 config.bind('<', 'tab-prev')
 config.bind('>', 'tab-next')
 config.bind('!', 'tab-focus 1')
+config.bind('@', 'tab-focus 2')
 config.bind('#', 'tab-focus 3')
 config.bind('$', 'tab-focus 4')
 config.bind('%', 'tab-focus 5')
+config.bind('^', 'tab-focus 6')
 config.bind('&', 'tab-focus 7')
+config.bind('*', 'tab-focus 8')
 config.bind('(', 'tab-focus 9')
 config.bind(')', 'tab-focus 10')
-config.bind('*', 'tab-focus 8')
 config.bind(',M', 'hint links spawn mpv {hint-url}')
 config.bind(',m', 'spawn mpv {url}')
-config.bind(',s', 'scroll-px 0 500')
 config.bind(',r', 'spawn --userscript readability')
-config.bind('@', 'tab-focus 2')
-config.bind('^', 'tab-focus 6')
+config.bind(',S', 'hint links userscript proxify')
+config.bind(',s', 'spawn --userscript proxify')
 config.bind('yf', 'hint links yank')

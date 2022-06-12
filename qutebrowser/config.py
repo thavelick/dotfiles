83f1aeaf32022ad637788806bdf7f4f0e943333f
Tristan Havelick <tristan@havelick.com>
2022-06-12 10:10:37 -0600

    qutebrowser: add bindings to add things to wallabang and toggele animations

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -9,6 +9,8 @@ config.set('content.cookies.accept', 'all', 'devtools://*')
 config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}; rv:90.0) Gecko/20100101 Firefox/90.0', 'https://accounts.google.com/*')
 config.set('content.headers.user_agent', 'Mozilla/5.0 ({os_info}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99 Safari/537.36', 'https://*.slack.com/*')
 
+custom_stylesheet = '/home/tristan/Projects/dotfiles/qutebrowser/styles.css'
+config.set('content.user_stylesheets', custom_stylesheet)
 c.content.blocking.method = 'both'
 
 config.set('content.javascript.enabled', is_work)
@@ -25,6 +27,7 @@ js_sites = [
     'https://duckduckgo.com',
     'https://app.element.io',
     'https://web.archive.org',
+    'https://wallabag.tristanhavelick.com',
 ]
 
 for site in js_sites:
@@ -73,11 +76,13 @@ config.bind('&', 'tab-focus 7')
 config.bind('*', 'tab-focus 8')
 config.bind('(', 'tab-focus 9')
 config.bind(')', 'tab-focus 10')
+config.bind(',a', f'config-cycle content.user_stylesheets {custom_stylesheet} ""')
 config.bind(',M', 'hint links spawn mpv {hint-url}')
 config.bind(',m', 'spawn mpv {url}')
 config.bind(',r', 'spawn --userscript readability')
 config.bind(',S', 'hint links userscript proxify')
 config.bind(',s', 'spawn --userscript proxify')
+config.bind(',w', 'spawn /home/tristan/Projects/wallabag-tiny-cli/wallabag_tiny_cli.py add "{url}"')
 config.bind('yf', 'hint links yank')
 
 c.aliases = {'w': 'session-save', 'q': 'close', 'qa': 'quit', 'wq': 'quit --save', 'wqa': 'quit --save', '1': 'scroll-to-perc 0', '%': 'scroll-to-perc 100'}

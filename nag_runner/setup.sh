bc3a84f7c93becc488743c2e05aca99d4629e58c
Tristan Havelick <tristan@havelick.com>
2022-08-14 17:47:00 -0600

    do regular yay updates with nag runner

--- nag_runner/setup.sh
+++ nag_runner/setup.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 nag_runner_path="$HOME/Projects/nag-runner"
-
 [ ! -d "$nag_runner_path" ] && git clone git@github.com:thavelick/nag-runner.git $nag_runner_path
-
-
+[ -f "/etc/arch-release" ] && ln -svf $DOTFILES_HOME/nag_runner/nag_runner.arch.json $HOME/.config/nag_runner.json

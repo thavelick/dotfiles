291ef1550eb8c2796db408393196c3bbdaeefcff
Tristan Havelick <tristan@havelick.com>
2023-06-10 12:22:19 -0600

    Add auto-commit script to stage and commit changes with a generated commit message
    This commit adds a new file, `auto-commit.zsh`, which automates the process of staging and committing changes to a git repository. The script stages all changes, prompts the user to select which changes to commit, generates a commit message using an external script, and then commits the changes with the generated message. This will help to streamline the commit process and ensure that all commits have informative and useful messages.

---
+++ git/auto-commit.zsh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+echo "staging files"
+git add --intent-to-add .
+git add -p
+
+echo "generating commit message"
+diff=$(git diff --staged)
+commit_msg=$($HOME/Projects/ai-asker/ask.py "write a good commit message for this diff: $diff")
+
+echo $commit_msg
+git commit -m "$commit_msg"

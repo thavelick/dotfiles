c82a2c7530040b7f846209f4528dadb86466191e
Tristan Havelick <tristan@havelick.com>
2024-02-07 21:52:22 -0700

    use .desktop files to determine if they're gui apps instead of using pacman

--- river/launcher.sh
+++ river/launcher.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 program_to_launch=$(echo -n "$PATH" | xargs -d: -I{} -r find -L {} -maxdepth 1 -mindepth 1 -type f -executable -printf '%P\n' 2>/dev/null | sort -u | fzf)
-package=$(pkgfile -qb "$(which "$program_to_launch")" 2>/dev/null)
-dependencies=$(pacman -Qi "$package" 2>/dev/null | grep -oP 'Depends On.*' | cut -d: -f2)
-# If the program does NOT depend on a gui package, launch it with foot:
-if ! echo "$dependencies" | grep -qE 'gtk3|qt5|qt6|wayland'; then
-  program_to_launch="foot -- $(which "$program_to_launch")"
-fi
+desktop_file=$(grep -lE "Exec=.*$program_to_launch" /usr/share/applications/*.desktop)
+
+[ -z "$desktop_file" ] || grep -qE "Terminal=true" "$desktop_file" && program_to_launch="foot $(which "$program_to_launch")"
 riverctl spawn "$program_to_launch"

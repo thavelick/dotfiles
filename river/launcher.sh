c5ccfab75c07f0d6a36fdfe197db9ef96a3e3423
Tristan Havelick <tristan@havelick.com>
2024-02-08 18:54:09 -0700

    perfer frequently run programs in launcher

--- river/launcher.sh
+++ river/launcher.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-program_to_launch=$(echo -n "$PATH" | xargs -d: -I{} -r find -L {} -maxdepth 1 -mindepth 1 -type f -executable -printf '%P\n' 2>/dev/null | sort -u | fzf)
-desktop_file=$(grep -lE "Exec=.*$program_to_launch" /usr/share/applications/*.desktop)
+# get a list of all the programs in the PATH
+programs=$(echo -n "$PATH" | xargs -d: -I{} -r find -L {} -maxdepth 1 -mindepth 1 -type f -executable -printf '%P\n' 2>/dev/null | sort -u)
+# get the programs we've previously run, excluding any that don't exist anymore
+programs_from_history=$(grep -xFf <(echo -e "$programs") "$HOME/.cache/launcher_history" 2>/dev/null)
+# make a list of all the programs, sorted by the frequency they've been run via this launcher
+frequency_sorted_programs=$(echo -e "$programs\n$programs_from_history" | sort | uniq -c | sort -nr | awk '{print $2}')
 
+# let the user choose what to launch, saving what they choose to a history file for later runs
+program_to_launch=$(echo -e "$frequency_sorted_programs" | fzf)
+echo "$program_to_launch" >> "$HOME/.cache/launcher_history"
+
+# run the program they chose, in a terminal if needed
+desktop_file=$(grep -lE "Exec=.*$program_to_launch" /usr/share/applications/*.desktop)
 [ -z "$desktop_file" ] || grep -qE "Terminal=true" "$desktop_file" && program_to_launch="foot $(which "$program_to_launch")"
 riverctl spawn "$program_to_launch"

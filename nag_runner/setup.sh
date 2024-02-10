795f5c34378694ff4d1efc4e8568ce90b035b3df
Tristan Havelick <tristan@havelick.com>
2024-02-10 20:31:59 +0000

    configure nag runner for debian

--- nag_runner/setup.sh
+++ nag_runner/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
 nag_runner_path="$HOME/Projects/nag-runner"
-[ ! -d "$nag_runner_path" ] && git clone git@github.com:thavelick/nag-runner.git $nag_runner_path
-[ -f "/etc/arch-release" ] && ln -svf $DOTFILES_HOME/nag_runner/nag_runner.arch.json $HOME/.config/nag_runner.json
+[ ! -d "$nag_runner_path" ] && git clone git@github.com:thavelick/nag-runner.git "$nag_runner_path"
+nag_runner_config_file=""
+[ -f "/etc/arch-release" ] && nag_runner_config_file="$DOTFILES_HOME/nag_runner/nag_runner.arch.json"
+[ -f "/etc/debian_version" ] && nag_runner_config_file="$DOTFILES_HOME/nag_runner/nag_runner.debian.json"
+[ -n "$nag_runner_config" ] && ln -svf "$nag_runner_config_file" "$HOME/.config/nag_runner.json"

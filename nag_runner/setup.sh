34bb3e8659da07b85f1205f3eb7c5d0f2ff209bb
Tristan Havelick <tristan@havelick.com>
2024-02-10 13:42:35 -0700

    add nag runner for debian

--- nag_runner/setup.sh
+++ nag_runner/setup.sh
@@ -4,4 +4,5 @@ nag_runner_path="$HOME/Projects/nag-runner"
 nag_runner_config_file=""
 [ -f "/etc/arch-release" ] && nag_runner_config_file="$DOTFILES_HOME/nag_runner/nag_runner.arch.json"
 [ -f "/etc/debian_version" ] && nag_runner_config_file="$DOTFILES_HOME/nag_runner/nag_runner.debian.json"
-[ -n "$nag_runner_config" ] && ln -svf "$nag_runner_config_file" "$HOME/.config/nag_runner.json"
+[ -n "$nag_runner_config_file" ] && ln -svf "$nag_runner_config_file" "$HOME/.config/nag_runner.json"
+exit 0

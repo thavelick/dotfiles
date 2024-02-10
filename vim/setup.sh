6af6605c8507582141d2de7b21bb312aef118b77
Tristan Havelick <tristan@havelick.com>
2024-02-10 13:43:47 -0700

    install packer in a platform independent way

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+packer_path="$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
+[ ! -d "$packer_path" ] && mkdir -p "$packer_path" && git clone --depth 1 https://github.com/wbthomason/packer.nvim "$packer_path"
+echo "$packer_path"
 
 # symlink .config/nvim/init.vim
 mkdir -p "$HOME/.config/nvim"

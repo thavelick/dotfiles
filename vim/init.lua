f6059a4a813ea9cbd0c8562c69ba6cde8b9322c7
Tristan Havelick <tristan@havelick.com>
2024-02-24 05:33:19 -0700

    steramline keymaps

--- vim/init.lua
+++ vim/init.lua
@@ -31,27 +31,17 @@ vim.o.timeoutlen = 250
 vim.wo.number = true
 vim.wo.relativenumber = true
 
+-- Configure which-key
+local which_key = require('which-key')
+which_key.setup({})
+
 -- disable the arrow keys so I actually learn to use hjkl
-vim.api.nvim_set_keymap('n', '<up>', '<nop>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<down>', '<nop>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<left>', '<nop>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<right>', '<nop>', { noremap = true, silent = true })
-
-vim.api.nvim_set_keymap('v', '<up>', '<nop>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<down>', '<nop>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<left>', '<nop>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<right>', '<nop>', { noremap = true, silent = true })
-
-vim.api.nvim_set_keymap('i', '<up>', '<nop>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<down>', '<nop>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<left>', '<nop>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<right>', '<nop>', { noremap = true, silent = true })
+for _, key in ipairs({'<up>', '<down>', '<left>', '<right>'}) do
+  vim.keymap.set({'n', 'v', 'i'}, key, '<nop>', {noremap = true, silent = true})
+end
 
 -- map escape in normal mode to clear search highlights and close the quickfix window
-vim.api.nvim_set_keymap('n', '<esc>', ':nohlsearch<cr>:ccl<cr>', { noremap = true, silent = true })
-
--- Configure which-key
-require('which-key').setup({})
+vim.keymap.set('n', '<esc>', ':nohlsearch<cr>:ccl<cr>')
 
 -- Set colorscheme
 vim.cmd('colorscheme tokyonight')
@@ -91,11 +81,25 @@ vim.g.copilot_filetypes = {
 -- highlight all search matches
 vim.o.hlsearch = true
 
+-- Setup language servers manually
 
--- Setup language servers.
+-- Setup language servers with lspconfig
 local lspconfig = require('lspconfig')
 lspconfig.clangd.setup {}
-lspconfig.pyright.setup {}
+lspconfig.pyright.setup {
+  on_attach = function()
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0, desc = "Show hover documentation"})
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0, desc = "Go to definition"})
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = 0, desc = "Show references"})
+
+    which_key.register("<leader>d", { name = "Diagnostics" })
+    vim.keymap.set("n", "<leader>df", vim.diagnostic.goto_next, { buffer = 0, desc = "Go to next diagnostic"})
+    vim.keymap.set("n", "<leader>db", vim.diagnostic.goto_prev, { buffer = 0, desc = "Go to previous diagnostic"})
+    vim.keymap.set("n", "<leader>dl", vim.diagnostic.setloclist, { buffer = 0, desc = "Show diagnostics in loclist"})
+
+    vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { buffer = 0, desc = "Rename symbol"})
+  end,
+}
 lspconfig.tsserver.setup {}
 lspconfig.lua_ls.setup {
   settings = {
@@ -107,50 +111,12 @@ lspconfig.lua_ls.setup {
   },
 }
 
--- Global mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
-
--- Use LspAttach autocommand to only map the following keys
--- after the language server attaches to the current buffer
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, opts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>f', function()
-      vim.lsp.buf.format { async = true }
-    end, opts)
-  end,
-})
-
 -- Harpoon configuration
 local harpoon = require("harpoon")
 
 harpoon:setup()
 
-vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end, { desc = "Add current file to Harpoon list" })
 vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<C-t>", function() harpoon:list():select(1) end)

064dc8d50a9d6894048d54ad52f6e77fd79e84ff
Tristan Havelick <tristan@havelick.com>
2025-02-21 19:52:18 -0700

    fix up backups to work with new(ish) laptop.
    Reenable nag runner

--- backups.zsh
+++ backups.zsh
@@ -2,4 +2,4 @@
 timestamp=$(date +%s)
 sudo -E borg create --progress --exclude-from=/home/tristan/Projects/dotfiles/arch/backup_excludes.txt "/backuprepo/::system-$timestamp" /
 sudo rclone --config /home/tristan/.config/rclone/rclone.conf sync -P /backuprepo bernard:/media/disk2/backup/$(uname -n)
-sudo rclone --config /home/tristan/.config/rclone/rclone.conf sync --transfers=10 -P /backuprepo system-backup:thavelick-arch-backup
+sudo rclone --config /home/tristan/.config/rclone/rclone.conf sync --transfers=10 -P /backuprepo system-backup:thavelick-backup/$(uname -n)

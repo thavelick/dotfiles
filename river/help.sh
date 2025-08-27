1817ba4279bd678527ddf89083c3672f23f65901
Tristan Havelick <tristan@havelick.com>
2025-08-26 22:06:00 -0600

    Fix shellcheck warning: quote tput cols command substitution

--- river/help.sh
+++ river/help.sh
@@ -3,4 +3,4 @@
 
 # Wait for window to finish resizing before detecting terminal width
 sleep 0.1
-glow --pager -w $(tput cols) "$DOTFILES_HOME/river/README.md"
+glow --pager -w "$(tput cols)" "$DOTFILES_HOME/river/README.md"
